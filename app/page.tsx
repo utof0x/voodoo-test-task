4d63f94444b2ced407618b82cd98e3fbd6af31b8
Popovych Mykhailo <popovychmykhailo00@gmail.com>
2023-05-16 18:14:48 +0300

    created components + added absolute import for this folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { Header, Content, Footer } from "components";
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
 };
 
 export default HomePage;
